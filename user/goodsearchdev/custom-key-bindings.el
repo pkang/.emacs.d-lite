48e5b836a7cf5dcfe09e6320c35b6587185dce86
Daniel Jacobs and Sufyan Adam <pair+daniel.jacobs+sufyan.adam@goodsearch.com>
2014-03-21 11:11:44 -0700

    Set default font as Anonymice Powerline

---
+++ user/goodsearchdev/custom-key-bindings.el
@@ -0,0 +1,1 @@
+(global-set-key (kbd "s-T") 'rinari-find-rspec)
--- user/goodsearchdev/user-init.el
+++ user/goodsearchdev/user-init.el
@@ -8,3 +8,7 @@
 
 ;; appearance
 (load-theme 'monokai)
+
+;; Coding font
+(set-default-font "Anonymice Powerline-18")
+(add-to-list 'default-frame-alist '(font . "Anonymice Powerline-18"))
